9fec5eada763b375cc6a951e7faaa730f70e9c79
Li Yichuan <92080161+yclihkclr@users.noreply.github.com>
2025-05-09 14:01:44 +0800

    fix bug of fov check when up vector parallel to viewing direction

--- view_planning_brep.py
+++ view_planning_brep.py
@@ -228,7 +228,8 @@ def is_point_in_fov(vp, sp, negtive_surface_normal, connecting_direction, connec
         camera_z = -negtive_surface_normal  # Viewing direction
         camera_x = np.cross(up_vector, camera_z)
         if np.linalg.norm(camera_x) < 1e-5:
-            raise ValueError("Up vector is parallel to viewing direction.")
+            camera_x = np.array([1, 0, 0])
+            # raise ValueError("Up vector is parallel to viewing direction.")
         camera_x /= np.linalg.norm(camera_x)  # Normalize
         camera_y = np.cross(camera_z, camera_x)
 
@@ -248,7 +249,8 @@ def is_point_in_fov(vp, sp, negtive_surface_normal, connecting_direction, connec
         camera_z = negtive_surface_normal  # Viewing direction
         camera_x = np.cross(up_vector, camera_z)
         if np.linalg.norm(camera_x) < 1e-5:
-            raise ValueError("Up vector is parallel to viewing direction.")
+            camera_x = np.array([1, 0, 0])
+            # raise ValueError("Up vector is parallel to viewing direction.")
         camera_x /= np.linalg.norm(camera_x)  # Normalize
         camera_y = np.cross(camera_z, camera_x)
 
